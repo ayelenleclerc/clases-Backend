19eb16c9b77d7a251e741ae05097d2fa140442dc
ayelenleclerc <ayelenleclerc@gmail.com>
2022-09-21 08:56:21 -0300

    clase 9 lista

--- clase9/desafio2/index.js
+++ clase9/desafio2/index.js
@@ -9,23 +9,31 @@ app.engine("cte", async (path, option, callback) => {
   const content = await fs.readFile(path, "utf8");
   try {
     const html = content
-      .replace("^^titulo$$", `${option}.titulo`)
-      .replace("^^mensaje$$", `${option}.mensaje`)
-      .replace("^^autor$$", `${option}.autor`)
-      .replace("^^varsion$$", `${option}.version`);
+      .replace("^^titulo$$", `${option.titulo}`)
+      .replace("^^mensaje$$", `${option.mensaje}`)
+      .replace("^^autor$$", `${option.autor}`)
+      .replace("^^version$$", `${option.version}`);
     return callback(null, html);
-  } catch (err) {}
+  } catch (err) {
+    return callback(err);
+  }
 });
 app.set("views", "./views");
-app.set("views engine", "cte");
+app.set("view engine", "cte");
 
 app.get("/cte1", (req, res) => {
-  res.render("template", {
+  res.render("plantilla1", {
     titulo: "Clase de plantillas",
     mensaje: "Clase de plantillas, aprendiendo a usar plantillas",
-    autor: "Ayelen Leclerc  ",
-    version: 1.0,
+    autor: "Ayelen Leclerc",
+    version: "1.0.0",
   });
 });
 
-app.listen(PORT, () => console.log("ready on port " + PORT));
+const connectedServer = app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
+
+connectedServer.on("error", (error) => {
+  console.error("Error: ", error);
+});
