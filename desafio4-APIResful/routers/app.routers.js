9dee89983244d6042d123893f2560ec9cc1c4da5
ayelenleclerc <ayelenleclerc@gmail.com>
2022-10-13 15:36:38 -0300

    solo clases y ejemplos en clase

--- desafio4-APIResful/routers/app.routers.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express("routers");
-
-const productsRoutes = require("./products/products.routes");
-router.use("/products", productsRoutes);
-module.exports = router;
