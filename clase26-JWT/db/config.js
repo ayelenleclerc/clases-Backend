357c978886fe71bf76c58d092ac817b673dae1ca
ayelenleclerc <ayelenleclerc@gmail.com>
2022-11-30 09:13:09 -0300

    clase 26 clone repo

---
+++ clase26-JWT/db/config.js
@@ -0,0 +1,9 @@
+const env = require("../env.config");
+
+module.exports = {
+  mongodb: {
+    connectTo: (database) =>
+      `mongodb+srv://Ayelenleclerc:${DB_PASSWORD}@backend.xrrgkdz.mongodb.net/${database}?retryWrites=true&w=majority`,
+  },
+  // Change here for your mongo atlas account's URI
+};
